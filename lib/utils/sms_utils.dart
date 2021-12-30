acbe424460dbb65fd37210e33d9b0805394b43b9
tentone <tentone@outlook.com>
2021-12-30 10:07:41 +0000

    Telephony as SMS sender

--- lib/utils/sms_utils.dart
+++ lib/utils/sms_utils.dart
@@ -1,49 +1,46 @@
-import 'package:sms_advanced/sms_advanced.dart';
+import 'package:telephony/telephony.dart';
 
 /// Utils to send and receive SMS messages.
 class SMSUtils {
+  static Telephony telephony = Telephony.instance;
+
   /// Listen to incoming SMS messages.
   static void listen() {
-    SmsReceiver receiver = SmsReceiver();
-    if(receiver.onSmsReceived != null) {
-      receiver.onSmsReceived?.listen((SmsMessage ?msg){
-        print(msg?.body);
-      });
-    }
+    telephony.listenIncomingSms(
+        onNewMessage: (SmsMessage msg) {
+          print(msg.address);
+          print(msg.body);
+        },
+    );
   }
 
   /// Send a SMS to an address (phone number)
   static void send(String content, String address) async {
-    SmsSender sender = SmsSender();
-
-    SmsMessage message = SmsMessage(address, content);
-    message.onStateChanged.listen((state) {
-      if (state == SmsMessageState.Sent) {
-        print("SMS is sent!");
-      } else if (state == SmsMessageState.Delivered) {
-        print("SMS is delivered!");
-      }
-    });
-    sender.sendSms(message);
+    telephony.sendSms(
+        to: address,
+        message: content,
+        statusListener: (SendStatus status) {
+          print(status);
+        }
+    );
   }
 
   static Future getReceived(String address) async {
-    SmsQuery query = SmsQuery();
-    List<SmsMessage> messages = await query.querySms(
-      kinds: [SmsQueryKind.Inbox],
-      address: address
-    );
-
-    for(int i = 0; i < messages.length; i++) {
-      print(messages[i].address);
-      print(messages[i].body);
-    }
+    // List<SmsMessage> messages = await telephony.getInboxSms(
+    //   kinds: [SmsQueryKind.Inbox],
+    //   address: address
+    // );
+    //
+    // for(int i = 0; i < messages.length; i++) {
+    //   print(messages[i].address);
+    //   print(messages[i].body);
+    // }
   }
 
   /// Get all SMS received by the device.
   static Future getAll() async {
-    SmsQuery query = SmsQuery();
-    List<SmsMessage> messages = await query.getAllSms;
+    // SmsQuery query = SmsQuery();
+    List<SmsMessage> messages = await telephony.getInboxSms();
 
     for(int i = 0; i < messages.length; i++) {
       print(messages[i].address);
