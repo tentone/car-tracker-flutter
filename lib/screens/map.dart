e1196b9f2c42f4479733848e630b7f3adc725160
tentone <tentone@outlook.com>
2022-01-28 21:55:06 +0000

    Derp

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,6 +1,11 @@
+import 'dart:typed_data';
+
+import 'package:cartracker/data/tracker.dart';
 import 'package:cartracker/database/database.dart';
+import 'package:cartracker/database/tracker_db.dart';
 import 'package:cartracker/database/tracker_position_db.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -18,45 +23,79 @@ class MapScreen extends StatefulWidget {
 class MapScreenState extends State<MapScreen> {
   late MapboxMapController controller;
 
-  void onMapCreated(MapboxMapController controller) {
+  /// Method called when map is created to draw the tracker locations.
+  ///
+  /// Trackers are coded by their color
+  Future<void> onMapCreated(MapboxMapController controller) async {
     this.controller = controller;
+    this.controller.onSymbolTapped.remove(onSymbolTapped);
+
+    Database? db = await DataBase.get();
+    List<TrackerLastPosition> entries = await TrackerPositionDB.getAllTrackerLastPosition(db!);
+
+    for (int i = 0; i < entries.length; i++) {
+      this.controller.addSymbol(
+        SymbolOptions(
+          iconColor: Color(entries[i].tracker.color).toHexStringRGB(),
+          geometry: LatLng(entries[i].position.latitude, entries[i].position.longitude),
+          iconImage: 'airport-15',
+          textField: entries[i].tracker.name,
+          textSize: 14,
+          textOffset: const Offset(0, 0.8),
+          iconSize: 2,
+          textAnchor: 'top',
+          textColor: '#000000',
+          textHaloBlur: 1,
+          textHaloColor: '#ffffff',
+          textHaloWidth: 0.8,
+        ),
+      );
+    }
+
+  }
+
+  /// Adds an asset image to the currently displayed style
+  Future<void> addImageFromAsset(String name, String assetName) async {
+    final ByteData bytes = await rootBundle.load(assetName);
+    final Uint8List list = bytes.buffer.asUint8List();
+    return controller.addImage(name, list);
+  }
+
+  /// Method called when a tracker is pressed
+  ///
+  /// Open the tracker location in external application.
+  void onSymbolTapped(Symbol symbol) {
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-          future: () async {
-            Database? db = await DataBase.get();
-            return TrackerPositionDB.getAllTrackerLastPosition(db!);
-          }(),
-          builder: (BuildContext context, AsyncSnapshot<List<TrackerLastPosition>> entries) {
-            if (entries.data == null) {
-              return const SizedBox();
-            }
-
-            // TODO <REMOVE THIS>
-            print(entries.data);
-
-            return Stack(
-              children: <Widget>[
-                MapboxMap(
-                  accessToken: Global.MAPBOX_TOKEN,
-                  trackCameraPosition: true,
-                  initialCameraPosition: const CameraPosition(target: LatLng(35.0, 135.0), zoom: 5),
-                  onMapCreated: onMapCreated,
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                ),
-              ],
-            );
+      body: Stack(
+        children: <Widget>[
+          MapboxMap(
+            accessToken: Global.MAPBOX_TOKEN,
+            trackCameraPosition: true,
+            initialCameraPosition: const CameraPosition(target: LatLng(35.0, 135.0), zoom: 5),
+            onMapCreated: onMapCreated,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Database? db = await DataBase.get();
+          List<Tracker> trackers = await TrackerDB.list(db!);
+          for(int i = 0; i < trackers.length; i++) {
+            trackers[i].requestLocation();
           }
-      )
+        },
+        child: const Icon(Icons.gps_not_fixed),
+      ),
     );
-    
-
   }
 }
