e61305bf666a71df39de2b9e8d65ce24f2aa980e
tentone <tentone@outlook.com>
2022-02-22 12:00:07 +0000

    Drive helper

---
+++ lib/database/database_backup.dart
@@ -0,0 +1,10 @@
+/// Database backup class contains auxiliary classes to export and import database to external services.
+///
+/// Services are managed by the user (e.g. Google Driver, iCloud).
+class DatabaseBackup {
+  static exportGoogleDrive() async {
+    DriveHelper driveHelper = DriveHelper();
+
+  }
+
+}
