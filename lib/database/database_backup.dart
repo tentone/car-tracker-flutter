5bf7eeb3485533597ab1a282c980939118ca9c94
tentone <tentone@outlook.com>
2022-02-27 17:25:54 +0000

    Comment out drive

--- lib/database/database_backup.dart
+++ lib/database/database_backup.dart
@@ -1,24 +1,21 @@
-import 'package:drive_helper/drive_helper.dart';
-
-
 /// Database backup class contains auxiliary classes to export and import database to external services.
 ///
 /// Services are managed by the user (e.g. Google Driver, iCloud).
 class DatabaseBackup {
   /// Export database to google drive
   static exportGoogleDrive() async {
-    DriveHelper drive = DriveHelper();
-    await drive.signInAndInit([DriveScopes.app]);
-
-    DateTime now = DateTime.now();
-
-    drive.createFile('car_tracker_' + now.year.toString() + now.month.toString() + now.day.toString() + ".db", "application/x-sqlite3");
+    // DriveHelper drive = DriveHelper();
+    // await drive.signInAndInit([DriveScopes.app]);
+    //
+    // DateTime now = DateTime.now();
+    //
+    // drive.createFile('car_tracker_' + now.year.toString() + now.month.toString() + now.day.toString() + ".db", "application/x-sqlite3");
   }
 
   /// Import database to google drive
   static importGoogleDrive() async {
-    DriveHelper drive = DriveHelper();
-    await drive.signInAndInit([DriveScopes.app]);
+    // DriveHelper drive = DriveHelper();
+    // await drive.signInAndInit([DriveScopes.app]);
 
     // drive.openFile(fileID);
   }
